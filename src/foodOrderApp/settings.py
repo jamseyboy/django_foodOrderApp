bb5ee8f3d1fb3ca221d5ee34b46f507a56d24c1c
jamseyboy <buongahrangchal@gmail.com>
2024-11-08 08:45:15 +0530

    neon postgres implemented

--- src/foodOrderApp/settings.py
+++ src/foodOrderApp/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import datetime
 from decouple import config
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -100,6 +101,15 @@ DATABASES = {
     }
 }
 
+DATABASE_URL = config("DATABASE_URL", cast=str, default=None)
+if DATABASE_URL != "":
+    DATABASES={
+        "default":
+            dj_database_url.config(
+            default = DATABASE_URL,
+            conn_max_age=300
+        )
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
